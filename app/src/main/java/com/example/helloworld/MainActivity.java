8c5cf5fe953b6766f58ff7113bc71d8b1bac6bac
viniassuncao1 <viniciusmassuncao@gmail.com>
2025-08-05 10:32:12 -0300

    Criacao App

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+    }
+}
